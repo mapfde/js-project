57b421a574d165af86dda41be48d91a2d6458483
LibrarianGirl <AnnaWe95@t-online.de>
2016-05-04 13:12:48 +0200

    Flower und Himmel

--- program.js
+++ program.js
@@ -1,4 +1,46 @@
 function myFunction() {
 document.getElementById("mytitle").innerHTML = "Dieser Text wurde vom Skript geschrieben";
+goToXY(20,20);
+setPenSizeTo(1);
+//setPenColorTo("#F3FF05");
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
+penUp ()
+for (var Sprung = 1; Sprung <= 15; Sprung++) {
+	var randomX = Math.floor(Math.random() * 361) -180
+	var randomY = 		Math.floor(Math.random() * 271) -135
+	goToXY(randomX,randomY);
+  setPenColorTo(getRandomColor());
+  penDown();
+	for (var zahl = 1; zahl <= 5; zahl++) {
+		move (50);
+		turnRight (144); 
+  }
+  penUp ();
+}
+
+}
+
+function Flower() {
+	goToXY(20,20);
+setPenSizeTo(1);
+setPenColorTo("#9C0303");
+penDown();
+for (var zahl = 1; zahl <= 25; zahl++) {
+	for (var kreis = 1; kreis <= 360; kreis++) {
+		move (1);
+		turnRight (1); 
+  }
+	turnLeft (15);
+}
+
+}
+
+
 
